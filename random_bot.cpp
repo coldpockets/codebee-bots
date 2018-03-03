3fcd1f0d0d85d6846b05f53a8b9c1f11ce83ec94
William <mawilliamyan@gmail.com>
2018-03-03 15:20:59 -0500

    Updated random bot to actually be random bot again.

--- random_bot.cpp
+++ random_bot.cpp
@@ -6,37 +6,34 @@
 
 using namespace std;
 
-class MyBot : public Bot {
+class RandomBot : public Bot {
 public:
-    MyBot() : Bot("MyBot") { }
+    RandomBot() : Bot("RandomBot") { }
 
 protected:
     vector<Action> getMoves(int id, const Map& curMap) {
         vector<Action> moves;
 
-        /***************************************************************
-        	REPLACE CODE HERE
-        ****************************************************************/
-
         for (auto it = beeCells.begin(); it != beeCells.end(); ++it) {
             shared_ptr<Bee> bee = (*it)->getBee();
             int move = rand() % 5;
-            moves.push_back(Action(actionType::MOVE, (*it)->getPosition(), move, move > 0 ? move - 1 : bee->face));
+            moves.push_back(Action(
+                actionType::MOVE,
+                (*it)->getPosition(),
+                move, move > 0 ? move - 1 /* Faces move direction. */ : bee->face
+            ));
         }
-
-        /*************************************************************/
 
         return moves;
     }
 };
 
-/* DO NOT TOUCH - unless you know what you're doing ;) */
 int main() {
     srand(time(0));
 
     cout.sync_with_stdio(false);
 
-    MyBot myBot = MyBot();
+    RandomBot randomBot = RandomBot();
 
-    myBot.run();
+    randomBot.run();
 }
